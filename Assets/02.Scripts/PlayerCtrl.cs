cc027bcce02c774c1b2e16f7a2285483e40c569c
HereisLuvika <kth882500@gmail.com>
2025-01-22 15:20:43 +0900

    aniPlus

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    [SerializeField] private Transform tr;
+    private Transform tr;
     public float moveSpeed = 10f;
+    public float turnSpeed = 80f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +18,14 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        Debug.Log($"h = {h}");
-        Debug.Log($"v = {v}");
+        float r = Input.GetAxis("Mouse X");
 
-        Vector3 moveDirction = new Vector3(h, 0, v);
-        tr.Translate(moveDirction * Time.deltaTime * moveSpeed);
+        Vector3 moveDirction = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDirction.normalized * Time.deltaTime * moveSpeed);
 
+        // Vector3 moveDirction = new Vector3(h, 0, v);
+        // tr.Translate(moveDirction.normalized * Time.deltaTime * moveSpeed);
+
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
