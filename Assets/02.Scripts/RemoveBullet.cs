7d8e4150ab2a9c9454f390a0b4a133d94e37db77
HereisTaeHyun <kth882500@gmail.com>
2025-02-06 14:51:31 +0900

    EffectPlus

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     // 충돌 시작시 발생하는 이벤트
 
     private void OnCollisionEnter(Collision coll)
     {
-        // tag 바교
+        // tag 비교
         if(coll.collider.CompareTag("BULLET"))
         {
-            // 충돌 대상체 삭제
+            // 충돌 지점 정보 추출
+            ContactPoint cp = coll.GetContact(0);
+            Quaternion rotate = Quaternion.LookRotation(-cp.normal);
+            // 충돌 대상체 스파크 동적 생성 후 삭제
+            GameObject spark = Instantiate(sparkEffect, cp.point, rotate);
+            Destroy(spark, 0.5f);
             Destroy(coll.gameObject);
         }
     }
