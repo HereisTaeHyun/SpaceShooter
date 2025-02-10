bbf7afdf1c98b45cc458652e72ce8ac86b39bd6e
HereisTaeHyun <kth882500@gmail.com>
2025-02-10 14:49:16 +0900

    DamageZone 추가

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -13,11 +13,13 @@ public class BarrelCtrl : MonoBehaviour
 
     public Texture[] textures; // 무작위로 적용할 텍스쳐 배열
     public GameObject explosionEffect;
+    public float radius = 10.0f;
 
     private new MeshRenderer renderer; // 자식 오브젝트의 meshRenderer 지정
     private Transform tf;
     private Rigidbody rb;
     private int hitCount = 0;
+    private Collider[] colls = new Collider[10];
     // Start is called before the first frame update
     void Start()
     {
@@ -46,13 +48,32 @@ public class BarrelCtrl : MonoBehaviour
     // 폭파 함수
     void Explosion()
     {
-        // 폭파 효과 생성 후 5초 후 제거
+        // 폭파 효과 생성 후 5초 후 오브젝트 제거
         GameObject explosion = Instantiate(explosionEffect, transform.position, Quaternion.identity);
         Destroy(explosion, DELETE_TIME_EFFECT);
 
         // gameObject 무게를 가볍게 하고 Addforce로 날린 후 제거
-        rb.mass = MASS_BARREL;
-        rb.AddForce(Vector3.up * FORCE);
+        // rb.mass = MASS_BARREL;
+        // rb.AddForce(Vector3.up * FORCE);
+
+        // 간접 폭발력 전달
+        InDirectDamage(transform.position);
         Destroy(gameObject, DELETE_TIME_BARREL);
+    }
+
+    void InDirectDamage(Vector3 pos)
+    {
+        // 인근 드럼통 추출, 추출 시작 위치, 추출 범위, 레이어로 비트 연산자로 표현
+        Physics.OverlapSphereNonAlloc(pos, radius, colls, 1 << 3);
+        foreach(var coll in colls)
+        {
+            // 범위 내 collider의 rb 추출 후 mass 감소 및 축 고정 해제
+            // 이후 AddExplosionForce러 원 모양으로 반발력 생성
+            if(coll == null) continue;
+            rb = coll.GetComponent<Rigidbody>();
+            rb.mass = MASS_BARREL;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddExplosionForce(FORCE, pos, radius, 1200.0f);
+        }
     }
 }
