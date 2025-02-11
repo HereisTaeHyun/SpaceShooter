471db8690a51feb8d9f56969a2690ecc23f28e5f
HereisTaeHyun <kth882500@gmail.com>
2025-02-11 11:48:03 +0900

    Sound 추가

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 삭제 안되도록 하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
     public Transform firePos;
-    // Update is called once per frame
+    public AudioClip fireSfx;
+
+    // 오디오 컴포넌트
+    private new AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();   
+    }
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -18,5 +28,6 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {
         Instantiate(bullet, firePos.position, firePos.rotation);
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
